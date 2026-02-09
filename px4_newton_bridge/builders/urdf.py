9dc58bddcb9a77950b9143073c66a7235a1898c3
Peter Breuer <peter.breuer.profiles@gmail.com>
2026-02-09 11:01:49 +0100

    fix: URDF orientation. Base link is FRD but Newton ModelBuilder up axis is Z so we need a transform

--- px4_newton_bridge/builders/urdf.py
+++ px4_newton_bridge/builders/urdf.py
@@ -7,13 +7,13 @@ from .builder_base import BuilderBase
 
 
 class URDFBuilder(BuilderBase):
-    """Build a vehicle model from a URDF file.
-
-    Expects the URDF and its meshes to be in a ``urdf/`` subdirectory
-    of the vehicle folder.
-    """
 
     def build(self, builder: newton.ModelBuilder) -> None:
+        """Build a vehicle model from a URDF file.
+
+        Expects the URDF and its meshes to be in a ``urdf/`` subdirectory of the vehicle folder.
+        """
+
         urdf_dir = self.vehicle_dir / "urdf"
         urdf_files = glob.glob(str(urdf_dir / "*.urdf"))
         if len(urdf_files) != 1:
@@ -23,7 +23,9 @@ class URDFBuilder(BuilderBase):
 
         builder.add_urdf(
             urdf_files[0],
-            # xform=wp.transform_identity(),
+            xform=wp.transform(
+                wp.vec3(0, 0, 0), wp.quat_from_axis_angle(wp.vec3(1, 0, 0), wp.pi)
+            ),
             floating=True,
             # collapse_fixed_joints=True,
             ignore_inertial_definitions=False,
